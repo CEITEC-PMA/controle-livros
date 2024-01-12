7f28f5539c639e74112e8a3391f477a436498311
Adriano Barbosa <adriano.barbosa100@email.com>
2024-01-11 23:09:07 -0300

    att

---
+++ src/controllers/unidade.controller.js
@@ -0,0 +1,53 @@
+const httpStatus = require('http-status');
+const pick = require('../utils/pick');
+const ApiError = require('../utils/ApiError');
+const catchAsync = require('../utils/catchAsync');
+const { userService } = require('../services');
+
+const createUnidade = catchAsync(async (req, res) => {
+  const user = await userService.createUser(req.body);
+  res.status(httpStatus.CREATED).send(user);
+});
+
+const getUsers = catchAsync(async (req, res) => {
+  const filter = pick(req.query, ['name', 'role']);
+  const options = pick(req.query, ['sortBy', 'limit', 'page']);
+  const result = await userService.queryUsers(filter, options);
+  res.send(result);
+});
+
+const getUser = catchAsync(async (req, res) => {
+  const user = await userService.getUserById(req.params.userId);
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+  }
+  res.send(user);
+});
+
+const getUserConnected = catchAsync(async (req, res) => {
+  const { inep } = req.user;
+  const user = await userService.getUserByInep(inep);
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+  }
+  res.send(user);
+});
+
+const updateUser = catchAsync(async (req, res) => {
+  const user = await userService.updateUserById(req.params.userId, req.body);
+  res.send(user);
+});
+
+const deleteUser = catchAsync(async (req, res) => {
+  await userService.deleteUserById(req.params.userId);
+  res.status(httpStatus.NO_CONTENT).send();
+});
+
+module.exports = {
+  createUnidade,
+  getUsers,
+  getUser,
+  getUserConnected,
+  updateUser,
+  deleteUser,
+};
---
+++ src/validations/unidade.validation.js
@@ -0,0 +1,63 @@
+const Joi = require('joi');
+const { password, objectId } = require('./custom.validation');
+
+const createUnidade = {
+  body: Joi.object().keys({
+    name: Joi.string().required(),
+    inep: Joi.string().required(),
+    fone: Joi.string().required(),
+    endereco: {
+      cep: Joi.string().required(),
+      logradouro: Joi.string().required(),
+      quadra: Joi.string().optional().allow(''),
+      lote: Joi.string().optional().allow(''),
+      complemento: Joi.string().optional().allow(''),
+      bairro: Joi.string().optional().allow(''),
+      localidade: Joi.string().required(),
+      uf: Joi.string().required(),
+    },
+  }),
+};
+
+const getUsers = {
+  query: Joi.object().keys({
+    name: Joi.string(),
+    role: Joi.string(),
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+  }),
+};
+
+const getUser = {
+  params: Joi.object().keys({
+    userId: Joi.string().custom(objectId),
+  }),
+};
+
+const updateUser = {
+  params: Joi.object().keys({
+    userId: Joi.required().custom(objectId),
+  }),
+  body: Joi.object()
+    .keys({
+      email: Joi.string().email(),
+      password: Joi.string().custom(password),
+      name: Joi.string(),
+    })
+    .min(1),
+};
+
+const deleteUser = {
+  params: Joi.object().keys({
+    userId: Joi.string().custom(objectId),
+  }),
+};
+
+module.exports = {
+  createUnidade,
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+};
